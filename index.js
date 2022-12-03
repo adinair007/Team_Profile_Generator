34eadfc63bbebe2df35b2fdf0ffb86d22190c39e
Aditya Nair <adinair.in@gmail.com>
2022-12-02 20:06:43 -0600

    Added prompts to index.js

--- index.js
+++ index.js
@@ -1,10 +1,69 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const pageTemplate = require('./src/page_template');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const pageTemplate = require("./src/page_template");
 
-const employee = require('./lib/Employee');
-const engineer = require('./lib/Engineer');
-const intern = require('./lib/Intern');
-const manager = require('./lib/Manager');
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const manager = require("./lib/Manager");
 
-const 
+const newMembers = [];
+
+const questions = async () => {
+  const answers = await inquirer.prompt([
+    
+    //--Getting employee position--
+    {
+      type: "list",
+      name: "role",
+      message: "What is the member's role?",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+
+    //--Getting employee name--
+    {
+      type: "input",
+      name: "name",
+      message: "Please provide the member's full name.",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log(
+            "Invalid response. Please provide the member's full name."
+          );
+          return false;
+        };
+      },
+    },
+
+    //--Getting employee id number--
+    {
+        type:'input',
+        name: 'id',
+        message: "Please provide employee ID number.",
+        validate: (idInput) => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log('Invalid ID. Please provide a valid employee ID number.');
+                return false;
+            };
+        },
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please provide the member's email address.",
+        validate: (emailInput) => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Invalid email. Please provide a valid email address');
+            };
+        }
+    },
+
+  ]);
+};
